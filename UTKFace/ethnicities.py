de554066e6954588ec92f5f9fbab6ea9aced286c
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-04-18 11:30:25 -0400

    adding new models

---
+++ UTKFace/ethnicities.py
@@ -0,0 +1,101 @@
+import tensorflow as tf
+import aisquared
+import click
+
+def build_model():
+    input_layer = tf.keras.layers.Input((256, 256, 3))
+    x = tf.keras.layers.Conv2D(
+        32,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(input_layer)
+    x = tf.keras.layers.Conv2D(
+        32,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    x = tf.keras.layers.MaxPool2D()(x)
+    x = tf.keras.layers.Conv2D(
+        64,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        64,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    x = tf.keras.layers.MaxPool2D()(x)
+    x = tf.keras.layers.Conv2D(
+        128,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        256,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    x = tf.keras.layers.MaxPool2D()(x)
+    x = tf.keras.layers.Conv2D(
+        256,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    x = tf.keras.layers.Conv2D(
+        256,
+        3,
+        padding = 'same',
+        activation = 'relu'
+    )(x)
+    x = tf.keras.layers.MaxPool2D()(x)
+    x = tf.keras.layers.Flatten()(x)
+    for _ in range(3):
+        x = tf.keras.layers.Dense(256, activation = 'relu')(x)
+    output_layer = tf.keras.layers.Dense(5, activation = 'softmax')(x)
+
+    model = tf.keras.models.Model(input_layer, output_layer)
+    model.compile(
+        loss = 'sparse_categorical_crossentropy',
+        optimizer = 'adam',
+        metrics = ['accuracy']
+    )
+    return model
+
+@click.command()
+@click.argument('directory', type = click.Path(exists = True, file_okay = False, dir_okay = True))
+def main(directory):
+    model = build_model()
+    model.summary()
+    
+    train_dataset = tf.keras.utils.image_dataset_from_directory(
+        directory,
+        batch_size = 256,
+        validation_split = 0.2,
+        seed = 12,
+        subset = 'training'
+    )
+    test_dataset = tf.keras.utils.image_dataset_from_directory(
+        directory,
+        batch_size = 256,
+        validation_split = 0.2,
+        seed = 12,
+        subset = 'validation'
+    )
+    callback = tf.keras.callbacks.EarlyStopping(
+        monitor = 'val_loss',
+        patience = 5,
+        min_delta = 0.02
+    )
+    model.fit(train_dataset, epochs = 100, validation_data = test_dataset, callbacks = [callback])
+
+if __name__ == '__main__':
+    main()
+    
