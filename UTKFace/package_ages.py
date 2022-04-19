271ae7b120d2b0c2aa04a81e33241cb38b587439
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-04-19 08:45:22 -0400

    adding scripts to make air files

---
+++ UTKFace/package_ages.py
@@ -0,0 +1,27 @@
+import aisquared
+import click
+
+@click.command()
+@click.argument('model-file', type = click.Path(exists = True, file_okay = True, dir_okay = False))
+@click.option('--dtype', type = str, default = 'float32')
+def main(model_file, dtype):
+    harvester = aisquared.config.harvesting.ImageHarvester()
+    preprocesser = aisquared.config.preprocessing.ImagePreprocessor(
+        aisquared.config.preprocessing.Resize([256, 256])
+    )
+    analytic = aisquared.config.analytic.LocalModel(model_file, 'cv')
+    postprocesser = aisquared.config.postprocessing.Regression()
+    renderer = aisquared.config.rendering.ImageRendering()
+    feedback = aisquared.config.feedback.RegressionFeedback()
+    config = aisquared.config.ModelConfiguration(
+        'AgePredicter'
+        harvester,
+        preprocesser,
+        analytic,
+        postprocesser,
+        renderer,
+        feedback
+    ).compile(dtype = dtype)
+
+if __name__ == '__main__':
+    main()
---
+++ UTKFace/package_ethnicities.py
@@ -0,0 +1,37 @@
+import aisquared
+import click
+
+@click.command()
+@click.argument('model-file', type = click.Path(exists = True, file_okay = True, dir_okay = False))
+@click.option('--dtype', type = str, default = 'float32')
+def main(model_file, dtype):
+    label_map = [
+        'White',
+        'Black',
+        'Asian',
+        'Other'
+    ]
+    harvester = aisquared.config.harvesting.ImageHarvester()
+    preprocesser = aisquared.config.preprocessing.ImagePreprocessor(
+        aisquared.config.preprocessing.Resize([256, 256])
+    )
+    analytic = aisquared.config.analytic.LocalModel(model_file, 'cv')
+    postprocesser = aisquared.config.postprocessing.BinaryClassification(
+        label_map
+    )
+    renderer = aisquared.config.rendering.ImageRendering()
+    feedback = aisquared.config.feedback.MulticlassFeedback(
+        label_map
+    )
+    config = aisquared.config.ModelConfiguration(
+        'EthnicityPredicter'
+        harvester,
+        preprocesser,
+        analytic,
+        postprocesser,
+        renderer,
+        feedback
+    ).compile(dtype = dtype)
+
+if __name__ == '__main__':
+    main()
