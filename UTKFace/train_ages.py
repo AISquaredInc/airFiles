1ae632f39781b391b77d530fb60575bf9f573322
Jacob Renn <77127228+jacobrenn@users.noreply.github.com>
2022-05-22 07:18:47 -0400

    updating script to have generator training

--- UTKFace/train_ages.py
+++ UTKFace/train_ages.py
@@ -1,6 +1,35 @@
 import tensorflow as tf
+import numpy as np
 import aisquared
 import click
+import os
+
+def training_generator(directory, batch_size = 512):
+    files = os.listdir(directory)
+    np.random.shuffle(files)
+
+    ages = [int(f.split('_')[0]) for f in files]
+    m = min(ages)
+    M = max(ages)
+    del ages
+
+    i = 0
+    while True:
+        batch, labels = [], []
+        for _ in range(batch_size):
+            if i >= len(files):
+                np.random.shuffle(files)
+                i = 0
+            img = tf.keras.preprocessing.image.load_img(
+                os.path.join(directory, files[i]),
+                target_size = (256, 256)
+            )
+            img = np.array(img)/255
+            label = (int(files[i].split('_')[0]) - M)/(m - M)
+            batch.append(img)
+            labels.append(label)
+            i += 1
+        yield np.asarray(batch), np.asarray(labels)
 
 def build_model():
     input_layer = tf.keras.layers.Input((256, 256, 3))
@@ -70,30 +99,18 @@ def build_model():
 
 @click.command()
 @click.argument('directory', type = click.Path(exists = True, file_okay = False, dir_okay = True))
-def main(directory):
+@click.option('--batch-size', '-b', type = int, default = 64)
+def main(directory, batch_size):
     model = build_model()
     model.summary()
     
-    train_dataset = tf.keras.utils.image_dataset_from_directory(
-        directory,
-        batch_size = 256,
-        validation_split = 0.2,
-        seed = 12,
-        subset = 'training'
-    )
-    test_dataset = tf.keras.utils.image_dataset_from_directory(
-        directory,
-        batch_size = 256,
-        validation_split = 0.2,
-        seed = 12,
-        subset = 'validation'
-    )
+    train_dataset = training_generator(directory, batch_size)
     callback = tf.keras.callbacks.EarlyStopping(
-        monitor = 'val_loss',
+        monitor = 'loss',
         patience = 5,
-        min_delta = 0.02
+        min_delta = 0.001
     )
-    model.fit(train_dataset, epochs = 100, validation_data = test_dataset, callbacks = [callback])
+    model.fit(train_dataset, epochs = 100, callbacks = [callback], steps_per_epoch = len(os.listdir(directory))//batch_size)
     model.save('age_model.h5')
 
 if __name__ == '__main__':
